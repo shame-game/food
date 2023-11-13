43683b65c4c1d68d87abf722004de2fc84b6c4f1
shame-game <hoanghung.vam1209@gmail.com>
2023-11-13 10:33:37 +0700

    d

---
+++ js/main.js
@@ -0,0 +1,104 @@
+const b_box = document.querySelectorAll('.b-box');
+const hiddan = document.querySelectorAll('.bg-hiddan');
+const v_box = document.querySelectorAll('.v-box');
+
+console.log(v_box);
+console.log(b_box);
+
+b_box.forEach((box, index) => {
+    const hienbg = hiddan[index];
+    box.onclick = function () {
+        hienbg.setAttribute('style','display: flex')
+    }
+});
+
+v_box.forEach((card, index) => {
+    const hienbgc = hiddan[index];
+    card.onclick = function () {
+        hienbgc.setAttribute('style','display: none')
+    }
+});
+
+
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
+
