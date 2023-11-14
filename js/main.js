6b7ece85fc5a24abb53406de8a71a62cde9927aa
shame-game <hoanghung.vam1209@gmail.com>
2023-11-14 16:03:47 +0700

    e

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-const b_box = document.querySelectorAll('.b-box');
-const hiddan = document.querySelectorAll('.bg-hiddan');
-const v_box = document.querySelectorAll('.v-box');
+const btttt = document.querySelectorAll('.btttt');
+const bg = document.querySelectorAll('.bg');
+const box = document.querySelectorAll('.box');
+const clost = document.querySelectorAll('.clost')
 
-console.log(v_box);
-console.log(b_box);
-
-b_box.forEach((box, index) => {
-    const hienbg = hiddan[index];
-    box.onclick = function () {
-        hienbg.setAttribute('style','display: flex')
+btttt.forEach((bt, index) => {
+    const bgg = bg[index];
+    const boxx = box[index];
+    bt.onclick = function () {
+        bgg.setAttribute('style', 'display: flex');
+        boxx.setAttribute('style', 'display: flex');
+        document.querySelector('body').setAttribute('style','overflow: hidden;')
     }
 });
 
-v_box.forEach((card, index) => {
-    const hienbgc = hiddan[index];
-    card.onclick = function () {
-        hienbgc.setAttribute('style','display: none')
+bg.forEach((btt, index) => {
+    const boxx = box[index];
+    btt.onclick = function () {
+        btt.setAttribute('style', 'display: none');
+        boxx.setAttribute('style', 'display: none');
+        document.querySelector('body').setAttribute('style','overflow: auto;')
+    }
+});
+
+clost.forEach((bttt, index) => {
+    const bggg = bg[index];
+    const boxxx = box[index];
+    bttt.onclick = function () {
+        boxxx.setAttribute('style', 'display: none');
+        bggg.setAttribute('style','display: none');
+        document.querySelector('body').setAttribute('style','overflow: auto;')
     }
 });
 
